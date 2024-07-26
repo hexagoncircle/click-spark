0d548391eca3b77eb3be230b0cc24dae2a482a58
Ryan Mulligan <hexagoncircle@gmail.com>
2024-07-26 07:34:18 -0700

    Contain click in parent of custom element

--- click-spark.js
+++ click-spark.js
@@ -1,24 +1,22 @@
 class ClickSpark extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({ mode: "open" });
-    this.root = document.documentElement;
     this.svg;
-  }
-
-  get activeEls() {
-    return this.getAttribute("active-on");
+    this.attachShadow({ mode: "open" });
   }
 
   connectedCallback() {
     this.setupSpark();
+    this.parentNode.addEventListener("click", this.handleClick.bind(this));
+  }
 
-    this.root.addEventListener("click", (e) => {
-      if (this.activeEls && !e.target.matches(this.activeEls)) return;
+  detachedCallback() {
+    this.removeEventListener("click", this.handleClick);
+  }
 
-      this.setSparkPosition(e);
-      this.animateSpark();
-    });
+  handleClick(e) {
+    this.setSparkPosition(e);
+    this.animateSpark();
   }
 
   animateSpark() {
@@ -51,19 +49,13 @@ class ClickSpark extends HTMLElement {
   }
 
   setSparkPosition(e) {
-    let rect = this.root.getBoundingClientRect();
-
-    this.svg.style.left = e.clientX - rect.left - this.svg.clientWidth / 2 + "px";
-    this.svg.style.top = e.clientY - rect.top - this.svg.clientHeight / 2 + "px";
+    this.svg.style.left = e.clientX - this.svg.clientWidth / 2 + "px";
+    this.svg.style.top = e.clientY - this.svg.clientHeight / 2 + "px";
   }
 
   setupSpark() {
     let template = `
-      <style>
-        :host {
-          display: contents;
-        }
-        
+      <style>        
         svg {
           pointer-events: none;
           position: absolute;
